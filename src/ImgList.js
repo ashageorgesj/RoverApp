697d15d82b413e32a4b045bd83ed0f0999064d16
Asha George <ashageor@gmail.com>
2022-01-16 17:37:56 -0800

    Adding Files.

---
+++ src/ImgList.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Image from './Image.js';
+function ImgList(props){
+	
+	//return <div>{props.images.map((image)=><div key={image.id}><img src={image.img_src}/></div>)}</div>
+	return <div>{props.images.map((image)=><div key={image.id}><Image image={image}/></div>)}</div>
+
+}
+
+
+export default ImgList;
