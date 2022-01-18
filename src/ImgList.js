899c4fa117938159505afdf4c644569f59f1752f
Asha George <ashageor@gmail.com>
2022-01-17 17:46:03 -0800

    Changes for deployment.

--- src/ImgList.js
+++ src/ImgList.js
@@ -1,5 +1,7 @@
 import './App.css';
+import React,{Component} from 'react';
 import Image from './Image.js';
+
 function ImgList(props){
 	
 	//return <div>{props.images.map((image)=><div key={image.id}><img src={image.img_src}/></div>)}</div>
